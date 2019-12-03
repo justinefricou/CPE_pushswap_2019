f71a96dacf9d833af9b092a9b07284b506679c43
Justine Fricou <jfricou@justine.fricou>
2019-12-03 22:55:22 +0100

    ADDED [pushswap.h] Added header with functions' prototypes and list structure

---
+++ include/pushswap.h
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** pushswap.h
+** File description:
+** pushswap : header
+*/
+
+#ifndef DEF_PUSHSWAP
+#define DEF_PUSHSWAP
+
+typedef struct list {
+    struct list *previous;
+    struct list *next;
+    int nb;
+} list;
+
+int detect_errors(int argc, char **argv);
+int get_list(list *l_a);
+void display_sorting(list *l_a);
+void sa(list *l_a);
+void ra(list *l_a);
+
+#endif //DEF_PUSHSWAP
