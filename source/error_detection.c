fed4532665de944d7a613611dab9cd7a0f68ea66
Justine Fricou <jfricou@justine.fricou>
2019-12-04 10:03:46 +0100

    MODIFIED [error_detection.c] Added functions to detect is a char is a sign or a digit

--- source/error_detection.c
+++ source/error_detection.c
@@ -14,3 +14,32 @@ int detect_errors(int argc, char **argv)
             return (84);
     }
 }
+
+int is_int(char *str)
+{
+    int i = 0;
+
+    if (is_sign(str[i]))
+        i++;
+    else if (!is_digit(str[i]))
+        return (0);
+    for ( ; str[i] != 0; i++) {
+        if (!is_digit(str[i]))
+            return (0);
+    }
+    return (1);
+}
+
+int is_sign(char c)
+{
+    if (c == '-' || c == '+')
+        return (1);
+    return (0);
+}
+
+int is_digit(char c)
+{
+    if ('0' <= c && c <= '9')
+        return (1);
+    return (0);
+}
