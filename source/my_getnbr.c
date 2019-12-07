a8848a005e78656a2139743b7179dd51976717cc
Justine Fricou <jfricou@justine.fricou>
2019-12-07 17:08:50 +0100

    MODIFIED [my_getnbr.c] Changed parameter's name : from s to str

--- source/my_getnbr.c
+++ source/my_getnbr.c
@@ -2,20 +2,20 @@
 ** EPITECH PROJECT, 2019
 ** my_getnbr
 ** File description:
-** Returning a number, sent to the function as a string.
+** pushswap : returns a number, sent to the function as a string.
 */
 
 #include "../include/pushswap.h"
 
-int my_getnbr(char *s)
+int my_getnbr(char *str)
 {
     int nbr = 0;
     int i = 0;
 
-    for ( ; s[i] == '-' || s[i] == '+' || s[i] == '0'; i++);
-    for ( ; s[i] != 0; i++)
-        nbr = nbr * 10 + (s[i] - 48);
-    if (s[0] == '-')
+    for ( ; str[i] == '-' || str[i] == '+' || str[i] == '0'; i++);
+    for ( ; str[i] != 0; i++)
+        nbr = nbr * 10 + (str[i] - 48);
+    if (str[0] == '-')
         nbr = nbr * (-1);
     return (nbr);
 }
