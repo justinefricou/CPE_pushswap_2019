7f16f7841368addcb31329c2a79adbb0ce6db5d9
Justine Fricou <jfricou@justine.fricou>
2019-12-09 15:33:36 +0100

    MODIFIED [dislay_sorting.c] Added \n when the list is already sorted

--- source/display_sorting.c
+++ source/display_sorting.c
@@ -12,10 +12,9 @@ int display_sorting(int *list, int length_list)
     if (is_sorted(list, length_list))
         return (0);
     for (int i = 0; i < length_list; i++) {
-        if (is_sorted(list, length_list)) {
-            write(1, "\n", 1);
+        if (is_sorted(list, length_list))
             return (0);
-        } else if (i != 0)
+        else if (i != 0)
             write(1, " ", 1);
         go_through_list(list, length_list);
     }
@@ -28,6 +27,7 @@ int is_sorted(int *list, int length_list)
         if (list[i] > list[i + 1])
             return (0);
     }
+    write(1, "\n", 1);
     return (1);
 }
 
